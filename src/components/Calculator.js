bbacf243905081b1f66146e9f881043117ff0f39
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-04 17:33:05 +0100

    Removed the this key word in functional components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -24,7 +24,7 @@ const Calculator = () => {
 
   const { total, next, operation } = input;
   return (
-    <div className="calculator-container" onChange={this.handleDisplay}>
+    <div className="calculator-container" onChange={handleDisplay}>
       <div className="answer-div">
         <span>
           {total}
@@ -33,33 +33,33 @@ const Calculator = () => {
         </span>
       </div>
       <div className="first-div">
-        <button type="button" className="btn clear-all number" onClick={this.handleClick}>AC</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>+/-</button>
-        <button type="button" className="btn modulus number" onClick={this.handleClick}>%</button>
-        <button type="button" className="btn divide operator" onClick={this.handleClick}>÷</button>
+        <button type="button" className="btn clear-all number" onClick={handleClick}>AC</button>
+        <button type="button" className="btn number" onClick={handleClick}>+/-</button>
+        <button type="button" className="btn modulus number" onClick={handleClick}>%</button>
+        <button type="button" className="btn divide operator" onClick={handleClick}>÷</button>
       </div>
       <div className="second-div">
-        <button type="button" className="btn number" onClick={this.handleClick}>7</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>8</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>9</button>
-        <button type="button" className="btn multiplication operator" onClick={this.handleClick}>x</button>
+        <button type="button" className="btn number" onClick={handleClick}>7</button>
+        <button type="button" className="btn number" onClick={handleClick}>8</button>
+        <button type="button" className="btn number" onClick={handleClick}>9</button>
+        <button type="button" className="btn multiplication operator" onClick={handleClick}>x</button>
       </div>
       <div className="third-div">
-        <button type="button" className="btn number" onClick={this.handleClick}>4</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>5</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>6</button>
-        <button type="button" className="btn substraction operator" onClick={this.handleClick}>-</button>
+        <button type="button" className="btn number" onClick={handleClick}>4</button>
+        <button type="button" className="btn number" onClick={handleClick}>5</button>
+        <button type="button" className="btn number" onClick={handleClick}>6</button>
+        <button type="button" className="btn substraction operator" onClick={handleClick}>-</button>
       </div>
       <div className="fourth-div">
-        <button type="button" className="btn number" onClick={this.handleClick}>1</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>2</button>
-        <button type="button" className="btn number" onClick={this.handleClick}>3</button>
-        <button type="button" className="btn addition operator" onClick={this.handleClick}>+</button>
+        <button type="button" className="btn number" onClick={handleClick}>1</button>
+        <button type="button" className="btn number" onClick={handleClick}>2</button>
+        <button type="button" className="btn number" onClick={handleClick}>3</button>
+        <button type="button" className="btn addition operator" onClick={handleClick}>+</button>
       </div>
       <div className="fourth-div">
-        <button type="button" className="btn number zero" onClick={this.handleClick}>0</button>
-        <button type="button" className="btn period number" onClick={this.handleClick}>.</button>
-        <button type="button" className="btn equals-to operator" onClick={this.handleClick}>=</button>
+        <button type="button" className="btn number zero" onClick={handleClick}>0</button>
+        <button type="button" className="btn period number" onClick={handleClick}>.</button>
+        <button type="button" className="btn equals-to operator" onClick={handleClick}>=</button>
       </div>
     </div>
   );
