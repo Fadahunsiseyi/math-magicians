d57721d787a9fe5a2db2cfcb1cb1df2486ceab52
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-11 18:34:49 +0100

    Update README.md, add tests for next,total and operation

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,7 +1,7 @@
 import calculate from './calculate';
 
 describe('calculate', () => {
-  it('should return the sum of two numbers', () => {
+  test('should return the sum of two numbers', () => {
     const result = calculate({
       total: 0,
       next: null,
@@ -9,8 +9,45 @@ describe('calculate', () => {
     }, 'AC');
     expect(result).toEqual({
       total: 0,
+      operation: null,
+      next: null,
+    });
+  });
+  test('click on first number', () => {
+    const obj = {
+      total: null,
       next: null,
       operation: null,
-    });
+    };
+    const result = calculate(obj, '5');
+    expect(result.next).toEqual('5');
+  });
+  test('click on operation', () => {
+    const obj = {
+      total: null,
+      next: 5,
+      operation: null,
+    };
+    const result = calculate(obj, '+');
+    expect(result.operation).toEqual('+');
+    expect(result.total).toEqual(5);
+  });
+  test('click on second number', () => {
+    const obj = {
+      total: 5,
+      next: null,
+      operation: '+',
+    };
+    const result = calculate(obj, '3');
+    expect(result.next).toEqual('3');
+  });
+  test('calculate the total number', () => {
+    const obj = {
+      total: 5,
+      next: 10,
+      operation: '+',
+    };
+    const result = calculate(obj, '=');
+    expect(result.total).toBe('15');
   });
 });
