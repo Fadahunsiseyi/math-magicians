2dca5f3a6382a75f619e13b579ee0ecc2e352587
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-08-11 18:17:45 +0430

    add calculator snapshot testing

--- src/components/components.test.js
+++ src/components/components.test.js
@@ -1,9 +1,16 @@
 import renderer from 'react-test-renderer';
 import React from 'react';
 import Home from '../pages/Home';
+import Calculator from '../pages/Calculator';
 
 it('renders correctly', () => {
   const testHome = renderer.create(<Home />);
+  const testJson = testHome.toJSON();
+  expect(testJson).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const testHome = renderer.create(<Calculator />);
   const testJson = testHome.toJSON();
   expect(testJson).toMatchSnapshot();
 });
